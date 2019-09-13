8011c7cbd6f7b44612b01787b2d95f177de6e313
Dominik Tilp <Dominik.Tilp@dixonscarphone.com>
2019-09-13 12:07:27 +0200

    countLiveNeighbours and first rule of GOL

---
+++ src/gol.js
@@ -0,0 +1,34 @@
+export default function next(input) {
+  const output = input.map((row) => row.map((cell) => cell));
+  /*
+    input.map(function(row) {
+      return row.map(function(cell) {
+        return cell
+      })
+    })
+  */
+
+  for (let i = 0; i < input.length; i += 1) {
+    for (let j = 0; j < input[i].length; j += 1) {
+      const liveNeighbours = countLiveNeighbours(input, i, j);
+      if (liveNeighbours < 2) {
+        output[i][j] = 0;
+      }
+    }
+  }
+
+  return output;
+}
+
+export function countLiveNeighbours(board, i, j) {
+  let count = 0;
+  for (let x = i - 1; x <= i + 1; x += 1) {
+    if (x < 0 || x >= board.length) { continue; } // eslint-disable-line no-continue
+    for (let y = j - 1; y <= j + 1; y += 1) {
+      if (y < 0 || y >= board[x].length) { continue; } // eslint-disable-line no-continue
+      if (x === i && y === j) { continue; } // eslint-disable-line no-continue
+      count += board[x][y];
+    }
+  }
+  return count;
+}
---
+++ src/gol.test.js
@@ -0,0 +1,62 @@
+import next, { countLiveNeighbours } from './gol';
+
+test('function next exist', () => {
+  expect(typeof next).toBe('function');
+});
+
+test('next return array of array', () => {
+  const input = [[]];
+  const output = next(input);
+  expect(Array.isArray(output)).toBe(true);
+  expect(Array.isArray(output[0])).toBe(true);
+});
+
+test('one live cell will die', () => {
+  const input = [[1]];
+  const expectedOutput = [[0]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+
+test('it will work', () => {
+  const input = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 0, 1],
+  ];
+  const expectedOutput = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [0, 0, 0],
+  ];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+
+
+describe('countLiveNeighbours', () => {
+  test('return 8 when all cells are live', () => {
+    const input = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+    const expectedOutput = 8;
+    expect(countLiveNeighbours(input, 1, 1)).toBe(expectedOutput);
+  });
+  test('return 8 when all cells are live', () => {
+    const input = [
+      [0, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
+    const expectedOutput = 3;
+    expect(countLiveNeighbours(input, 0, 0)).toBe(expectedOutput);
+  });
+  test('return 8 when all cells are live', () => {
+    const input = [
+      [0, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
+    const expectedOutput = 3;
+    expect(countLiveNeighbours(input, 3, 2)).toBe(expectedOutput);
+  });
+});
