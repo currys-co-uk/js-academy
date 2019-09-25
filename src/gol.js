a05fc1e8b17cf4b751d51d26348521fb25a18bee
Patrícia Jamrišková <pjamriskova@MacBook-Pro-uzivatela-Patricia.local>
2019-09-25 22:46:28 +0200

    add rules and unit tests

--- src/gol.js
+++ src/gol.js
@@ -1,3 +1,17 @@
+export function countLiveNeighbours(board, i, j) {
+  let count = 0;
+  for (let x = i - 1; x <= i + 1; x += 1) {
+    if (x < 0 || x >= board.length) { continue; } // eslint-disable-line no-continue
+    for (let y = j - 1; y <= j + 1; y += 1) {
+      if (y < 0 || y >= board[x].length) { continue; } // eslint-disable-line no-continue
+      if (x === i && y === j) { continue; } // eslint-disable-line no-continue
+      count += board[x][y];
+    }
+  }
+  return count;
+}
+
+
 export default function next(input) {
   const output = input.map((row) => row.map((cell) => cell));
   /*
@@ -11,24 +25,13 @@ export default function next(input) {
   for (let i = 0; i < input.length; i += 1) {
     for (let j = 0; j < input[i].length; j += 1) {
       const liveNeighbours = countLiveNeighbours(input, i, j);
-      if (liveNeighbours < 2) {
+      if ((liveNeighbours < 2) || (liveNeighbours > 3)) {
         output[i][j] = 0;
-      }
+      } else if (liveNeighbours === 3) {
+        output[i][j] = 1;
+      } else { output[i][j] = input[i][j]; }
     }
   }
 
   return output;
-}
-
-export function countLiveNeighbours(board, i, j) {
-  let count = 0;
-  for (let x = i - 1; x <= i + 1; x += 1) {
-    if (x < 0 || x >= board.length) { continue; } // eslint-disable-line no-continue
-    for (let y = j - 1; y <= j + 1; y += 1) {
-      if (y < 0 || y >= board[x].length) { continue; } // eslint-disable-line no-continue
-      if (x === i && y === j) { continue; } // eslint-disable-line no-continue
-      count += board[x][y];
-    }
-  }
-  return count;
 }
--- src/gol.test.js
+++ src/gol.test.js
@@ -18,21 +18,53 @@ test('one live cell will die', () => {
   expect(output).toEqual(expectedOutput);
 });
 
-test('it will work', () => {
-  const input = [
-    [0, 1, 1],
-    [1, 0, 1],
-    [1, 0, 1],
-  ];
-  const expectedOutput = [
-    [0, 1, 1],
-    [1, 0, 1],
-    [0, 0, 0],
-  ];
+test('Any live cell with fewer than two live neighbours dies, as if by underpopulation', () => {
+  const input = [[1, 0], [1, 0]];
+  const expectedOutput = [[0, 0], [0, 0]];
   const output = next(input);
   expect(output).toEqual(expectedOutput);
 });
 
+test('Any live cell with fewer than two live neighbours dies, as if by underpopulation && any live cell with two live neighbours lives on to the next generation', () => {
+  const input = [[0, 1, 0], [0, 1, 0], [0, 1, 0]];
+  const expectedOutput = [[0, 0, 0], [1, 1, 1], [0, 0, 0]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+
+test('Any live cell with three live neighbours lives on to the next generation', () => {
+  const input = [[1, 1], [1, 1]];
+  const expectedOutput = [[1, 1], [1, 1]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+
+test('Any live cell with fewer than two live neighbours dies, as if by underpopulation && any live cell with two or three live neighbours lives on to the next generation && any dead cell with exactly three live neighbours becomes a live cell', () => {
+  const input = [[0, 1, 0], [0, 0, 1], [0, 1, 0]];
+  const expectedOutput = [[0, 0, 0], [0, 1, 1], [0, 0, 0]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+
+test('Any live cell with more than three live neighbours dies, as if by overpopulation', () => {
+  const input = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+  const expectedOutput = [[1, 0, 1], [0, 0, 0], [1, 0, 1]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+
+test('Any dead cell with exactly three live neighbours becomes a live cell', () => {
+  const input = [[0, 1], [1, 1]];
+  const expectedOutput = [[1, 1], [1, 1]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
+test('it will work', () => {
+  const input = [[0, 1, 1], [1, 0, 1], [1, 0, 1]];
+  const expectedOutput = [[0, 1, 1], [1, 0, 1], [0, 0, 0]];
+  const output = next(input);
+  expect(output).toEqual(expectedOutput);
+});
 
 describe('countLiveNeighbours', () => {
   test('return 8 when all cells are live', () => {
@@ -41,21 +73,12 @@ describe('countLiveNeighbours', () => {
     expect(countLiveNeighbours(input, 1, 1)).toBe(expectedOutput);
   });
   test('return 8 when all cells are live', () => {
-    const input = [
-      [0, 1, 1],
-      [1, 1, 1],
-      [1, 1, 1],
-    ];
+    const input = [[0, 1, 1], [1, 1, 1], [1, 1, 1]];
     const expectedOutput = 3;
     expect(countLiveNeighbours(input, 0, 0)).toBe(expectedOutput);
   });
   test('return 8 when all cells are live', () => {
-    const input = [
-      [0, 1, 1],
-      [1, 1, 1],
-      [1, 1, 1],
-      [1, 1, 1],
-    ];
+    const input = [[0, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]];
     const expectedOutput = 3;
     expect(countLiveNeighbours(input, 3, 2)).toBe(expectedOutput);
   });
