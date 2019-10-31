429ef7026b259752db85ef52b289ae1e7336be5d
Martin.Kroupa <cd.jojn@gmail.com>
2019-10-31 08:17:30 +0100

    Better design and rendering

--- src/index.js
+++ src/index.js
@@ -2,19 +2,70 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import next from "./gol.js";
+import styled from 'styled-components'
 
 import "./styles.css";
+
+const Button = styled.button`
+  background: ${props => props.primary ? "#c3073F" : "white"};
+  color: ${props => props.primary ? "white" : "#c3073F"};
+
+  font-size: 1em;
+  margin: 5px;
+  padding: 5px 10px;
+  border: 2px solid #c3073F;
+  border-radius: 5px;
+`;
+
+const Input = styled.input`
+  background: "white";
+  color: "#c3073F";
+
+  font-size: 1em;
+  margin: 5px 5px;
+  padding: 5px 5px;
+  border: 2px solid #c3073F;
+  border-radius: 5px;
+`;
+
+const BoardDesk = styled.table`
+  background: #1A1A1D;
+  border-spacing: 0px;
+`;
+
+const CellAlive = styled.td`
+  width: 10px;
+  height: 10px;
+  background: #c3073F;
+  color: black;
+  border: 1px solid black;
+
+  &:hover {
+    background: #6F2232;
+    border: 1px solid black;
+  }
+`;
+
+const CellDead = styled.td`
+  width: 10px;
+  height: 10px;
+  background: #1A1A1D;
+  border: 1px solid black;
+
+  &:hover {
+    background: gray;
+    border: 1px solid black;
+  }
+`;
 
 class Row extends React.PureComponent {
   drawCell(evt) {
     if (this.props.row[evt.target.dataset.y]["state"] === 1) {
       this.props.row[evt.target.dataset.y]["state"] = 0;
-      evt.target.className = "mycell_dead";
-      return
+    } else {
+      this.props.row[evt.target.dataset.y]["state"] = 1;
     }
-    this.props.row[evt.target.dataset.y]["state"] = 1;
-    evt.target.className = "mycell_live";
-    this.render();
+    this.forceUpdate();
   }
 
   render() {
@@ -22,29 +73,24 @@ class Row extends React.PureComponent {
       <tr>
         {this.props.row.map(cell => {
           if (cell.state === 1) {
-           return <td className="mycell_live" onClick={evt => this.drawCell(evt)} data-x={cell.x} data-y={cell.y}></td>;
+           return <CellAlive onClick={evt => this.drawCell(evt)} data-x={cell.x} data-y={cell.y}></CellAlive>;
           }
-          return <td className="mycell_dead" onClick={evt => this.drawCell(evt)} data-x={cell.x} data-y={cell.y}></td>;
+          return <CellDead onClick={evt => this.drawCell(evt)} data-x={cell.x} data-y={cell.y}></CellDead>;
         })}
       </tr>
     );
   }
 }
 
-const tableStyles = {
-  background: "darkgrey",
-  color: "white"
-};
-
 function Board(props) {
   return (
-    <table style={tableStyles}>
+    <BoardDesk>
       <tbody>
         {props.boardState.map(row => (
           <Row row={row} />
         ))}
       </tbody>
-    </table>
+    </BoardDesk>
   );
 }
 
@@ -54,16 +100,12 @@ Board.propTypes = {
   ).isRequired
 };
 
-function Button(props) {
-  return <button onClick={props.onClick}>{props.title}</button>;
-}
-
 class GolApp extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       boardState: [],
-      cols: 20,
+      cols: 50,
       rows: 40,
       animation: false,
     }
@@ -95,7 +137,7 @@ class GolApp extends React.PureComponent {
      for(let i = 0; i<10; i++)
      {
       this.handleNextClick();
-      this.render();
+      this.forceUpdate();
      }
    }
 
@@ -106,7 +148,7 @@ class GolApp extends React.PureComponent {
         board[i] = [];
         for (var j = 0; j < currentState.cols; j++) {
           board[i][j] = [];
-          board[i][j]["state"] = Math.round(Math.random()*0.8);
+          board[i][j]["state"] = Math.round(Math.random()*0.7);
           board[i][j]["x"] = i;
           board[i][j]["y"] = j;
         }
@@ -138,17 +180,21 @@ class GolApp extends React.PureComponent {
   render() {
     return (
       <div>
-        {this.state.boardState && <Board boardState={this.state.boardState} />}
-        <br></br>
-        Board: 
-        <input type="edit" name="cols" title="Width" value={this.state.cols} size="3" onChange={evt =>this.updateInput(evt)}/> x 
-        <input type="edit" name="rows" title="Height" value={this.state.rows} size="3" onChange={evt =>this.updateInput(evt)}/>
-        <br></br>
-        <Button onClick={this.reset} title="Random game" />
-        <Button onClick={this.clear} title="Clear board" />
-        <Button onClick={this.handleNextClick} title="Next state" />
-        <Button onClick={this.animation} title="Start animation" />
-        <Button onClick={this.logs} title="LOG" />
+        <div>
+          {this.state.boardState && <Board boardState={this.state.boardState} />}
+        </div>
+        <div>
+          Board: 
+          <Input type="edit" name="cols" title="Width" value={this.state.cols} size="1" onChange={evt =>this.updateInput(evt)}/> x
+          <Input type="edit" name="rows" title="Height" value={this.state.rows} size="1" onChange={evt =>this.updateInput(evt)}/>
+          </div>
+          <div>
+            <Button onClick={this.reset} primary>Random game</Button>
+            <Button onClick={this.clear} primary>Clear board</Button>
+            <Button onClick={this.handleNextClick}>Next state</Button>
+            <Button onClick={this.animation}>Start/Stop animation</Button>
+            <Button onClick={this.logs} title="LOG" primary>Logs</Button>
+        </div>
       </div>
     );
   }
